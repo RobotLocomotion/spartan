204edf87280992c75e92a9f233c47722afa8c4c2
Pat Marion <james.patrick.marion@gmail.com>
2017-03-08 16:54:00 -0500

    update app shortcuts

--- apps/pr2/runapp.sh
+++ apps/pr2/runapp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd $(dirname $(realpath $0))
-drake-visualizer --script app.py --director-config $SPARTAN_SOURCE_DIR/models/pr2/director/director_config.json $*
+cd $SPARTAN_SOURCE_DIR/apps/pr2
+
+drake-visualizer --script app.py --script bhpnTranslator.py --director-config $SPARTAN_SOURCE_DIR/models/pr2/director/director_config.json $*
