5063c864b28a36dab50b5352ede4764e5890467a
Gregory Izatt <gizatt@mit.edu>
2017-03-20 15:32:02 -0400

    Split miqp mesh model detector into its own library

---
+++ src/ObjectDetection/src/miqp_mesh_model_detector.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <random>
+#include <unistd.h>
+#include <typeinfo>
+
+#include "drake/multibody/rigid_body_tree.h"
+
+#include "yaml-cpp/yaml.h"
+
+class MIQPMultipleMeshModelDetector {
+  public:
+    struct PointCorrespondence {
+      Vector3d scene_pt;
+      Vector3d model_pt;
+      int face_ind;
+      int scene_ind;
+      std::vector<Vector3d> model_verts;
+      std::vector<double> vert_weights;
+      std::vector<int> vert_inds;
+    };
+
+    struct ObjectDetection {
+      Eigen::Affine3d est_tf;
+      std::vector<PointCorrespondence> correspondences;
+      int obj_ind;
+    };
+
+    struct Solution {
+      std::vector<ObjectDetection> detections;
+      double objective;
+    };
+
+    MIQPMultipleMeshModelDetector(YAML::Node config);
+
+    std::vector<Solution> doObjectDetection(Eigen::Matrix3Xd scene_pts);
+
+    RigidBodyTree<double> & get_robot() {
+      return robot_;
+    }
+
+  private:
+    RigidBodyTree<double> robot_;
+    VectorXd q_robot_gt_;
+
+    YAML::Node config_;
+
+    int optNumRays_ = 10;
+    int optRotationConstraint_ = 4;
+    int optRotationConstraintNumFaces_ = 2;
+    bool optAllowOutliers_ = true;
+    double optPhiMax_ = 0.1;
+    bool optUseInitialGuess_ = false;
+    double optCorruption_ = 100.0;
+
+};
