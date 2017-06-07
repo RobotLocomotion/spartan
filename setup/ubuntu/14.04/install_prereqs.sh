49e5a1baa0991acec834cba83fc17651fc78f145
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-06-07 14:09:58 -0400

    install prereqs script for 14.04

---
+++ setup/ubuntu/14.04/install_prereqs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -x -u
+
+apt install \
+  build-essential \
+  cmake \
+  libboost-all-dev \
+  libopenni2-dev \
+  libopencv-dev \
+  libqhull-dev \
+  libqt4-dev \
+  libqwt-dev \
+  libusb-1.0-0-dev \
+  libvtk5-dev \
+  libvtk5-qt4-dev \
+  python-dev \
+  python-matplotlib \
+  python-numpy \
+  python-pip \
+  python-scipy \
+  python-vtk \
+  python-yaml \
