5926ab52d946e5bb483a6087aec746099d1f13ba
Gregory Izatt <gizatt@mit.edu>
2017-06-28 20:21:44 -0400

    GoICP with downsampling, and also revive plotting utils

--- src/ObjectDetection/src/common/common_vtk.hpp
+++ src/ObjectDetection/src/common/common_vtk.hpp
@@ -5,6 +5,8 @@
 #include <vtkOBJReader.h>
 #include <vtkPLYReader.h>
 #include <vtkPolyDataMapper.h>
+#include <vtkPolyDataPointSampler.h>
+#include <vtkCleanPolyData.h>
 #include <vtkProperty.h>
 #include <vtkSmartPointer.h>
 #include <vtkSTLReader.h>
@@ -50,4 +52,41 @@ static vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
     polyData = reader->GetOutput();
   }
   return polyData;
+}
+
+static Eigen::Matrix3Xd LoadAndDownsamplePolyData(const std::string fileName, double downsample_spacing = -1.0)
+{
+  vtkSmartPointer<vtkPolyData> cloudPolyData = ReadPolyData(fileName.c_str());
+  cout << "Loaded " << cloudPolyData->GetNumberOfPoints() << " points from " << fileName << endl;
+
+  vtkSmartPointer<vtkPolyData> cloudPolyDataOut;
+
+  if (downsample_spacing <= 0.0){
+    cloudPolyDataOut = cloudPolyData;
+    printf("... and not downsampling.\n");
+  } else {
+    vtkSmartPointer<vtkPolyDataPointSampler> pointSampler = vtkSmartPointer<vtkPolyDataPointSampler>::New();
+    pointSampler->SetDistance(downsample_spacing);
+    pointSampler->SetInput(cloudPolyData);
+    pointSampler->Update();
+    vtkSmartPointer<vtkPolyData> cloudPolyDataDownsampled = pointSampler->GetOutput();
+
+    printf("sampled but not cleaned\n");
+    vtkSmartPointer<vtkCleanPolyData> pointCleaner = vtkSmartPointer<vtkCleanPolyData>::New();
+    pointCleaner->SetToleranceIsAbsolute(true);
+    pointCleaner->SetAbsoluteTolerance(downsample_spacing);
+    pointCleaner->SetInput(cloudPolyDataDownsampled);
+    pointCleaner->Update();
+    cloudPolyDataOut = pointCleaner->GetOutput();
+
+    cout << "Downsampled to " << cloudPolyDataOut->GetNumberOfPoints() << " points" << endl;
+  }
+
+  Eigen::Matrix3Xd out_pts(3, cloudPolyDataOut->GetNumberOfPoints());
+  for (int i=0; i<cloudPolyDataOut->GetNumberOfPoints(); i++){
+    out_pts(0, i) = cloudPolyDataOut->GetPoint(i)[0];
+    out_pts(1, i) = cloudPolyDataOut->GetPoint(i)[1];
+    out_pts(2, i) = cloudPolyDataOut->GetPoint(i)[2];
+  }
+  return out_pts;
 }
