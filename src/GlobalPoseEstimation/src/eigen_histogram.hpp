06be80490621d3066f540455b636fa860bc38ce1
Gregory Izatt <gizatt@mit.edu>
2017-07-27 17:15:39 -0400

    Add eigen histogram generation utility

---
+++ src/GlobalPoseEstimation/src/eigen_histogram.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <vector>
+
+template <typename Scalar> class EigenHistogram {
+public:
+  EigenHistogram(
+      const Eigen::Ref<const Eigen::VectorXi> n_bins,
+      const Eigen::Ref<const Eigen::Matrix<Scalar, -1, 1>> lower_bounds,
+      const Eigen::Ref<const Eigen::Matrix<Scalar, -1, 1>> upper_bounds);
+  void Reset();
+  void AddData(const Eigen::Matrix<Scalar, -1, -1> &data);
+  const std::vector<int> &Serialize() { return histogram_data_; };
+
+  // Exposed for testing
+  int convert_full_index_to_linear_index(Eigen::VectorXi full_index);
+  Eigen::VectorXi convert_linear_index_to_full_index(int linear_index);
+
+private:
+  Eigen::VectorXi n_bins_;
+  Eigen::Matrix<Scalar, -1, 1> lower_bounds_;
+  Eigen::Matrix<Scalar, -1, 1> upper_bounds_;
+  std::vector<int> histogram_data_;
+};
---
+++ src/GlobalPoseEstimation/src/test_eigen_histogram.cpp
@@ -0,0 +1,56 @@
+#include "eigen_histogram.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+  VectorXi bins_many_different_dims(5);
+  for (int i = 0; i < 5; i++) {
+    bins_many_different_dims[i] = (rand() % 10) + 10;
+  }
+  VectorXd lb_many(5);
+  VectorXd ub_many(5);
+  EigenHistogram<double> test_histogram_big(bins_many_different_dims, lb_many,
+                                            ub_many);
+  for (int i = 0; i < bins_many_different_dims.prod(); i++) {
+    if (i !=
+        test_histogram_big.convert_full_index_to_linear_index(
+            test_histogram_big.convert_linear_index_to_full_index(i))) {
+      printf("Mismatch!\n");
+    }
+  }
+
+  Matrix<double, 1, 6> test_matrix_1d;
+  test_matrix_1d << 1.0, 2.0, 2.0, 3.0, 3.0, 3.0;
+
+  VectorXi bins_1d = VectorXi::Ones(1) * 3;
+  VectorXd lb_1d = VectorXd::Ones(1) * 0.5;
+  VectorXd ub_1d = VectorXd::Ones(1) * 3.5;
+
+  EigenHistogram<double> test_histogram_1d(bins_1d, lb_1d, ub_1d);
+  test_histogram_1d.AddData(test_matrix_1d);
+
+  cout << "1d: ";
+  for (auto const &c : test_histogram_1d.Serialize())
+    cout << c << ' ';
+  cout << endl;
+
+  Matrix<double, 2, 4> test_matrix_2d;
+  test_matrix_2d.col(0) = Vector2d(0.0, 0.0);
+  test_matrix_2d.col(1) = Vector2d(0.0, 1.0);
+  test_matrix_2d.col(2) = Vector2d(0.0, 1.0);
+  test_matrix_2d.col(3) = Vector2d(1.0, 1.0);
+
+  VectorXi bins_2d = VectorXi::Ones(2) * 2;
+  VectorXd lb_2d = VectorXd::Ones(2) * 0.0;
+  VectorXd ub_2d = VectorXd::Ones(2) * 1.0;
+
+  EigenHistogram<double> test_histogram_2d(bins_2d, lb_2d, ub_2d);
+  test_histogram_2d.AddData(test_matrix_2d);
+
+  cout << "2d: ";
+  for (auto const &c : test_histogram_2d.Serialize())
+    cout << c << ' ';
+  cout << endl;
+}
