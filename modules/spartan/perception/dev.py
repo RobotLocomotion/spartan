aa241e09a6505eea67209d9a3969b976d789fc38
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-10-10 12:32:36 -0400

    Revert "switch drake resource root to be the source tree"
    This reverts commit 21c8da4bf7e26e696c2694fd13b2dac28b72b851.

---
+++ modules/spartan/perception/dev.py
@@ -0,0 +1,88 @@
+#system
+import numpy as np
+import os
+
+#director
+from director import ioUtils
+from director import lcmUtils
+from director import lcmframe
+from director import transformUtils
+from director import robotsystem
+from director import segmentation
+from director import cameraview
+from director import pydrakeik
+from director import packagepath
+from director import roboturdf
+from director import robotlinkselector
+from director import fieldcontainer
+from director import framevisualization
+from director import drcargs
+from director import visualization as vis
+import bot_core as lcmbotcore
+
+
+# spartan
+import spartan.utils as spartanUtils
+
+
+#labelfusion
+import labelfusion.cameraposes
+
+def getFilenames(folderName, logName):
+    """
+    Parse some standard filenames into a dict given the logFolder
+    :param logFolder:
+    :return:
+    """
+    d = dict()
+    d['logname'] = os.path.join(folderName, logName)
+    d['cameraposes'] = os.path.join(folderName, "posegraph.posegraph")
+    d['reconstruction'] = os.path.join(folderName, "reconstructed_pointcloud.vtp")
+
+    return d
+
+
+
+class ElasticFusionReconstruction(object):
+
+	def __init__(self, folderName=None, logName="cup_on_table"):
+		if folderName is None:
+			sourceDir = spartanUtils.getSpartanSourceDir()
+			folderName = os.path.join(sourceDir, 'logs', 'test')
+
+		self.folderName = folderName
+		self.logName = logName
+		self.filenames = getFilenames(self.folderName, self.logName)
+		self.loadReconstructedPointCloud(self.filenames['reconstruction'])
+
+		self.cameraToWorldTransform = None
+		self.setupSubscribers()
+
+
+	def loadReconstructedPointCloud(self, filename=None):
+		if filename is None:
+			sourceDir = spartanUtils.getSpartanSourceDir()
+			filename = os.path.join(sourceDir, 'logs', 'test', 'reconstructed_pointcloud.vtp')
+
+		polyData = ioUtils.readPolyData(filename)
+		self.pointcloud = vis.showPolyData(polyData, 'reconstructed pointcloud', colorByName='RGB')
+		vis.addChildFrame(self.pointcloud)
+
+	def loadCameraPoses(self, filename=None):
+		if filename is None:
+			filename = self.filenames['cameraposes']
+
+		self.cameraPoses = labelfusion.cameraposes.CameraPoses(posegraphFile=filename)
+
+	def setupSubscribers(self):
+		lcmUtils.addSubscriber('OPENNI_FRAME_LEFT_TO_LOCAL', lcmbotcore.rigid_transform_t, self.onCameraToWorldTransform)
+
+	def onCameraToWorldTransform(self,msg):
+		# only do this once
+		if self.cameraToWorldTransform is not None:
+			return
+
+		self.cameraToWorldTransform = lcmframe.frameFromRigidTransformMessage(msg)
+		self.pointcloud.getChildFrame().copyFrame(self.cameraToWorldTransform)
+
+
---
+++ modules/spartan/perception/handeyecalibration.py
@@ -0,0 +1,193 @@
+#system
+import numpy as np
+import time
+import os
+import subprocess
+
+#director
+from director import lcmUtils
+from director import lcmframe
+from director import transformUtils
+from director import robotsystem
+from director import segmentation
+from director import cameraview
+from director import pydrakeik
+from director import packagepath
+from director import roboturdf
+from director import robotlinkselector
+from director import fieldcontainer
+from director import framevisualization
+from director import drcargs
+from director import visualization as vis
+from director import ikplanner
+from director import robotposegui
+RobotPoseGUIWrapper = ikplanner.RobotPoseGUIWrapper
+import bot_core as lcmbotcore
+
+from director.timercallback import TimerCallback
+
+
+
+
+# spartan
+import spartan.utils as spartanUtils
+
+
+class RobotService(object):
+
+	def __init__(self, robotSystem, removeFloatingBase=True):
+		self.robotSystem = robotSystem
+		self.manipPlanner = robotSystem.manipPlanner
+		self.removeFloatingBase = True
+		self.ikPlanner = robotSystem.ikPlanner
+
+	"""
+	Joint positions should be a dict of the form{'joint_name': joint_value}
+	"""
+	def movePose(self, joint_positions):
+		
+		assert isinstance(joint_positions, dict)
+
+		self.manipPlanner.lastPlan = None
+		startPose = self.robotSystem.robotStateJointController.q
+		endPose = self.ikPlanner.mergePostures(startPose, joint_positions)
+		plan = self.ikPlanner.computePostureGoal(startPose, endPose)
+		self.manipPlanner.commitManipPlan(plan)
+
+		# block until this plan is over
+		planDuration = plan.plan[-1].utime/1e6
+		print "running plan of duration %s seconds "  %(planDuration)
+		time.sleep(1.1*planDuration)
+
+	def getPose(self):
+		q = self.robotSystem.robotStateJointController.q
+		if self.removeFloatingBase:
+			q = q[6:]
+
+		return q
+		
+
+class HandEyeCalibration(object):
+
+	def __init__(self, robotSystem, handFrame='palm'):
+		self.robotSystem = robotSystem
+		self.robotService = RobotService(robotSystem)
+		self.handFrame = handFrame
+		self.setup()
+		self.calibrationData = None
+
+		self.timer = TimerCallback(targetFps=1)
+		self.timer.callback = self.callback
+        # self.timer.callback = self.callback
+
+
+	def setup(self):
+		self.nominalPose = 'center'
+		self.poseList = ['center', 'left', 'right', 'forwards', 'backwards']
+		self.groupName = 'Calibration'
+		poseNamesInCalibrationGroup = RobotPoseGUIWrapper.getPoseNamesInGroup(self.groupName)
+		print "names in calibration group ", poseNamesInCalibrationGroup
+		self.poseDict = dict()
+
+		for poseName in self.poseList:
+			d = dict()
+			d['nominal'] = RobotPoseGUIWrapper.getPose(self.groupName, poseName)
+
+			for extension in ['x', 'y', 'z']:
+				poseNameExtended = poseName + "_" + extension
+				if poseNameExtended in poseNamesInCalibrationGroup:
+					d[extension] = RobotPoseGUIWrapper.getPose(self.groupName, poseNameExtended)
+
+			self.poseDict[poseName] = d
+
+		
+
+
+	def captureDataAtPose(self, poseName=""):
+		data = dict()
+		data['pose_name'] = poseName
+		data['pose'] = self.robotService.getPose().tolist()
+		data['hand_frame_name'] = self.handFrame
+		handTransform = self.robotSystem.robotStateModel.getLinkFrame(self.handFrame)
+		data['hand_frame'] = spartanUtils.poseFromTransform(handTransform)
+		data['utime'] = self.robotSystem.robotStateJointController.lastRobotStateMessage.utime
+		return data
+
+	def testCaptureData(self):
+		data = self.captureDataAtPose()
+		self.calibrationData.append(data)
+
+	def moveHome(self):
+		self.robotService.movePose(self.poseDict['center']['nominal'])
+
+	def run(self):
+		self.calibrationData = []
+		self.moveHome()
+
+		for poseName in self.poseList:
+			
+			poses = self.poseDict[poseName]
+			nominal = poses['nominal']
+			self.robotService.movePose(nominal)
+			data = self.captureDataAtPose('nominal')
+			self.calibrationData.append(data)
+
+			for subPoseName, pose in poses.iteritems():
+				if subPoseName == 'nominal':
+					continue;
+
+				self.robotService.movePose(pose)
+				data = self.captureDataAtPose(poseName + "_" + subPoseName)
+				self.calibrationData.append(data)
+				self.robotService.movePose(nominal)
+
+			self.moveHome()
+			
+
+	def makePoseOrder(self):
+		self.poseOrder = []
+		for poseName in self.poseList:
+			poses = self.poseDict[poseName]
+			self.poseOrder.append(self.poseDict[poseName]['nominal'])
+			for subPoseName, pose in poses.iteritems():
+				if subPoseName == 'nominal':
+					continue;
+				self.poseOrder.append(self.poseDict[poseName][subPoseName])
+			
+			self.poseOrder.append(self.poseDict['center']['nominal'])
+
+	def callback(self):
+		self.testCaptureData()
+		if len(self.poseOrder) == 0:
+			print "finished data collection"
+			self.timer.stop()
+			self.loggerProcess.kill()
+			self.saveCalibrationData(os.path.join(self.calibrationFolderName, 'robot_data.yaml'))
+			return
+
+		
+		pose = self.poseOrder.pop(0)
+		self.robotService.movePose(pose)
+
+	def runCalibration(self):
+		self.calibrationData = []
+		unique_name = time.strftime("%Y%m%d-%H%M%S")
+		self.calibrationFolderName = os.path.join(spartanUtils.getSpartanSourceDir(), 'calibration_data',unique_name)
+		os.mkdir(self.calibrationFolderName)
+		os.chdir(self.calibrationFolderName)
+		cmd = "lcm-logger calibration.lcmlog"
+		self.loggerProcess = subprocess.Popen("exec " + cmd, shell = True)
+
+		self.makePoseOrder()
+		self.timer.start()
+
+	def test(self):
+		pose = self.poseDict['center']['nominal']
+		self.robotService.movePose(pose)
+
+	def saveCalibrationData(self, filename=None):
+		if filename is None:
+			filename = os.path.join(spartanUtils.getSpartanSourceDir(), 'sandbox', 'hand_eye_calibration_robot_data.yaml')
+
+
+		spartanUtils.saveToYaml(self.calibrationData, filename)
