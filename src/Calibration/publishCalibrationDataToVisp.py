42aee1c2fa21cf89bd11a0ef125ebe1c295c7cfc
Pete Florence <pete.florence@gmail.com>
2017-10-10 21:40:31 -0400

    publishCalibrationDataToVisp spits back computed result, not yet sure if correct

--- src/Calibration/publishCalibrationDataToVisp.py
+++ src/Calibration/publishCalibrationDataToVisp.py
@@ -9,6 +9,8 @@
 
 import rospy
 import yaml
+import time
+import random
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Transform
 from visp_hand2eye_calibration.msg import TransformArray
@@ -26,16 +28,36 @@ from visp_hand2eye_calibration.srv import compute_effector_camera
 # publish transforms for visp
 ###
 
+def randomTransform():
+	transform = Transform()
+	transform.translation.x = random.uniform(-1,1)
+	transform.translation.y = random.uniform(-1,1)
+	transform.translation.z = random.uniform(-1,1)
+	transform.rotation.x    = random.uniform(-1,1)
+	transform.rotation.y    = random.uniform(-1,1)
+	transform.rotation.z    = random.uniform(-1,1)
+	transform.rotation.w    = 1.0
+	return transform
+
+def applyCameraTransform(transform_in):
+	transform = Transform()
+	transform.translation.x = transform_in.translation.x
+	transform.translation.y = transform_in.translation.y
+	transform.translation.z = transform_in.translation.z + 0.5
+	transform.rotation.x    = transform_in.rotation.x   
+	transform.rotation.y    = transform_in.rotation.y   
+	transform.rotation.z    = transform_in.rotation.z   
+	transform.rotation.w    = transform_in.rotation.w   
+	return transform
+
 # gather world transforms
 world_effector_transforms = TransformArray()
 world_effector_transforms.header.seq = 0
 world_effector_transforms.header.stamp = 0
 world_effector_transforms.header.frame_id = "world"
 
-zero_transform = Transform()
-for i in range(1000):
-	world_effector_transforms.transforms.append(zero_transform)
-
+for i in range(10):
+	world_effector_transforms.transforms.append(randomTransform())
 
 # gather camera transforms
 camera_object_transforms = TransformArray()
@@ -43,23 +65,27 @@ camera_object_transforms.header.seq = 0
 camera_object_transforms.header.stamp = 0
 camera_object_transforms.header.frame_id = "world"
 
-zero_transform = Transform()
-for i in range(1000):
-	camera_object_transforms.transforms.append(zero_transform)
-
-
-print len(world_effector_transforms.transforms)
-print len(camera_object_transforms.transforms)
+for i,v in enumerate(world_effector_transforms.transforms):
+	camera_object_transforms.transforms.append(applyCameraTransform(v))
 
 # init node and publishers
 rospy.init_node('visp_calibration_data_publisher')
-world_effector_pub = rospy.Publisher('/world_effector', TransformArray, queue_size=100)
-camera_object_pub  = rospy.Publisher('/camera_object', TransformArray, queue_size=100)
+world_effector_pub = rospy.Publisher('/world_effector', Transform, queue_size=1000)
+camera_object_pub  = rospy.Publisher('/camera_object', Transform, queue_size=1000)
 
-# publish TransfromArrays
+# hack: need to sleep between advertising topic and publishing topic, otherwise
+# visp may not receive all of them
+time.sleep(2)
 
-world_effector_pub.publish(world_effector_transforms)
-camera_object_pub.publish(camera_object_transforms)
+# publish Transfroms
+
+for i in range(len(world_effector_transforms.transforms)):
+	world_effector_pub.publish(world_effector_transforms.transforms[i])
+	camera_object_pub.publish(camera_object_transforms.transforms[i])
+
+# hack: need to sleep after publishing all transforms, otherwise visp may not
+# be ready when it is asked to compute the hand-eye ("effector_camera") transform
+time.sleep(2)
 
 # initialize service proxy for the compute_effector_camera service
 rospy.wait_for_service('/compute_effector_camera')
@@ -69,6 +95,5 @@ try:
   print reply
 except rospy.ServiceException as exc:
   print("Service did not process request: " + str(exc))
-
 
 rospy.spin() # not actually necessary, just blocks
