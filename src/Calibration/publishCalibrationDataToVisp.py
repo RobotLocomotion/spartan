984eac0588f2e46d5beff3af0a9974cf91627c82
Pete Florence <pete.florence@gmail.com>
2017-10-11 12:43:00 -0400

    augment calibration data with correct camera frames

--- src/Calibration/publishCalibrationDataToVisp.py
+++ src/Calibration/publishCalibrationDataToVisp.py
@@ -20,6 +20,7 @@ from visp_hand2eye_calibration.srv import compute_effector_camera
 
 sys.path.append('../LabelFusion/modules/labelfusion')
 from cameraposes import CameraPoses
+from director import transformUtils
 
 
 path_to_calibration_folder = os.path.join(os.getcwd(),"data/20171011-145541")
@@ -34,31 +35,38 @@ print posegraph_file
 cameraposes = CameraPoses(posegraph_file)
 
 # read in available robot data
-yaml_file_name = os.path.join(path_to_calibration_folder, "robot_data.yaml")
-with open(yaml_file_name, 'r') as f:
+in_yaml_file_name = os.path.join(path_to_calibration_folder, "robot_data.yaml")
+with open(in_yaml_file_name, 'r') as f:
     calib_data = yaml.load(f)
 
 
 # augment data with matched camera poses
+# NOTE: camera poses are defined relative to first camera frame, hence the first one will look unintialized
 for index, value in enumerate(calib_data):
-	print index
-	print value['utime']
 	transform = cameraposes.getCameraPoseAtUTime(value['utime'])
-	print transform
+	(pos, quat) = transformUtils.poseFromTransform(transform)
+	calib_data[index]['camera-pose'] = dict()
+	calib_data[index]['camera-pose']['quaternion'] = dict()
+	calib_data[index]['camera-pose']['quaternion']['w'] = float(quat[0])
+	calib_data[index]['camera-pose']['quaternion']['x'] = float(quat[1])
+	calib_data[index]['camera-pose']['quaternion']['y'] = float(quat[2])
+	calib_data[index]['camera-pose']['quaternion']['z'] = float(quat[3])
+	calib_data[index]['camera-pose']['translation'] = dict()
+	calib_data[index]['camera-pose']['translation']['x'] = float(pos[0])
+	calib_data[index]['camera-pose']['translation']['y'] = float(pos[1])
+	calib_data[index]['camera-pose']['translation']['Z'] = float(pos[2])
 
 
-###
-# read in yaml file of camera poses and hand poses
-###
+out_yaml_file_name = os.path.join(path_to_calibration_folder, "robot_camera_data.yaml")
+with open(out_yaml_file_name, 'w') as outfile:
+    yaml.dump(calib_data, outfile, default_flow_style=False)
 
-
-###
-# compute relative transforms for all
-###
 
 ###
 # publish transforms for visp
 ###
+
+## --- testing with random transforms -- ##
 
 def randomTransform():
 	transform = Transform()
@@ -99,6 +107,12 @@ camera_object_transforms.header.frame_id = "world"
 
 for i,v in enumerate(world_effector_transforms.transforms):
 	camera_object_transforms.transforms.append(applyCameraTransform(v))
+
+## --- testing with random transforms -- ##
+
+#for index, value in enumerate(calib_data):
+
+
 
 # init node and publishers
 rospy.init_node('visp_calibration_data_publisher')
