6f7123c4c9a1f54d0194fd66a6c2d1bdc007a8a1
Gregory Izatt <gizatt@mit.edu>
2017-10-13 11:36:35 -0400

    Test with perception on (for sure)
    Shouldn't be a big change, as it already is on by default

--- setup/docker/run_jenkins.sh
+++ setup/docker/run_jenkins.sh
@@ -4,13 +4,22 @@ rm -rf build
 mkdir build
 cd build
 
-cmake ..
+cmake -DWITH_PERCEPTION:BOOL=ON ..
 exit_status=$?
 if [ ! $exit_status -eq 0 ]; then
   echo "Error code in CMake: " $exit_status
   exit $exit_status
 fi
 
+make -j8
+exit_status=$?
+if [ ! $exit_status -eq 0 ]; then
+  echo "Error code in make: " $exit_status
+  exit $exit_status
+fi
+
+# Try building *again* to ensure that re-installing various pieces doesn't
+# break (see e.g. issue #159)
 make -j8
 exit_status=$?
 if [ ! $exit_status -eq 0 ]; then
