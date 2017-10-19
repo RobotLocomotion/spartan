4b7ec7aa6f480c4648af816e55da62ceed2786d8
Gregory Izatt <gregory.izatt@gmail.com>
2017-10-19 15:00:24 -0400

    Add vertex sampling

--- src/SceneScanner/run_scene_scanner.cpp
+++ src/SceneScanner/run_scene_scanner.cpp
@@ -18,15 +18,48 @@
 
 #include "yaml-cpp/yaml.h"
 
+/**
+ * Given a mesh with good normals (in world frame),
+ * and a sequence of RGB images with good intrinsics
+ * and extrinsics in the frame of that mesh, generate
+ * a list of color observations made of that vertex from
+ * different perspectives. From this information, one could
+ * conceivably jointly estimate lighting and material properties.
+ *
+ * Specifically:
+ *   A vertex v transforms into a given camera frame by transformation:
+ *     [u, v, 1]^T  = K TF [v_x v_y v_z 1]^T
+ *   i.e. a standard pinhole camera model. TF is the camera pose, and
+ *   K is an intrinsics matrix mapping from 3D pose in camera frame onto
+ *   the image plane.
+ *
+ * First we populate the list of camera TFs across time, and store it separately
+ * (as a TF is relevant for all pixels in an image).
+ *
+ * For every vertex, we construct a vertex_data_points list the stores a list
+ * of observation events, each containing:
+ *   - Index into the camera extrinsic TF list (1 byte)
+ *   - RGB observation (3 bytes)
+ *
+ * Example approximate data usage computation:
+ *    ~250000 vertex mesh
+ *    ~4000 relevant different views into environment
+ *     3 channels (RGB)
+ *     = 3GB total data we'll feed into an optimization minimum.
+ *
+ *
+ *    Of course, this can be dramatically reduced by cropping the input mesh.
+ */
+
 DEFINE_string(config_filename, "config_filename",
               "YAML config file supplying search parameters.");
 
 DEFINE_string(mesh_filename, "mesh",
-              "VTP-openable mesh with colored vertices.");
+              "VTP-openable mesh with vertices with normals.");
 
 DEFINE_string(labelfusion_data_dir, "data_dir",
-              "Root of a LabelFusion log dir with a posegraph.posegraph and an "
-              "images folder.");
+              "Root of a LabelFusion log dir with a posegraph.posegraph file, "
+              "a info.yaml file with camera intrinsics, and an images folder.");
 // Assumption about labelfusion data:
 //  the ith row (1-indexed) of the posegraph file corresponds to the rgb file
 /// "%010d_rgb.png" % i
@@ -37,6 +70,7 @@ using Eigen::Vector4d;
 using Eigen::Vector3i;
 using Eigen::VectorXd;
 using Eigen::Matrix3Xd;
+using Eigen::Matrix3d;
 using Eigen::MatrixXd;
 using Eigen::Isometry3d;
 
@@ -78,6 +112,87 @@ vector<Isometry3d> GeneratePoseList(string posegraph_filename) {
   return tfs;
 }
 
+/**
+ * Generates a camera instrinsic matrix from a config file
+ * that supplies the center X and Y of the image plane,
+ * and the focal length.
+ * Generates K =
+ *    [ f  0  u_x]
+ *    [ 0  f  u_y]
+ *    [ 0  0  1]
+ * i.e. assumes 0 skew, and assumes focal length is supplied
+ * in pixel units already.
+ */
+Matrix3d GenerateCameraIntrinsics(string config_filename) {
+  auto console = spdlog::get("console");
+  Matrix3d K;
+  K.setZero();
+
+  YAML::Node config = YAML::LoadFile(config_filename);
+  if (config["intrinsics"]) {
+    double c_x = config["intrinsics"]["principalX"].as<double>();
+    double c_y = config["intrinsics"]["principalY"].as<double>();
+
+    K(0, 2) = c_x;
+    K(1, 2) = c_y;
+
+    double f = config["intrinsics"]["focalLength"].as<double>();
+    K(0, 0) = f;
+    K(1, 1) = f;
+
+  } else {
+    console->error("Config file had no intrinsics!");
+    exit(1);
+  }
+
+  return K;
+}
+
+struct ColorObservation {
+  int tf_index;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+};
+struct VertexDataSet {
+  Vector3d v;
+  Vector3d n;
+  std::vector<ColorObservation> color_observations;
+};
+
+void InitializeVertexDataSets(const vtkSmartPointer<vtkPolyData> meshPolyData,
+                              const double bounding_box[6],
+                              std::vector<VertexDataSet>* vertexDataSets,
+                              std::map<int, int>* vertIndToDatasetInd) {
+  auto console = spdlog::get("console");
+
+  vertexDataSets->resize(meshPolyData->GetNumberOfPoints());
+  vertIndToDatasetInd->clear();
+
+  vtkDataArray* normalsGeneric = meshPolyData->GetPointData()->GetNormals();
+
+  // Populate with some subset of vertices
+  int k_good_vertices = 0;
+  for (int i = 0; i < meshPolyData->GetNumberOfPoints(); i++) {
+    auto vtk_pt = meshPolyData->GetPoint(i);
+
+    if (vtk_pt[0] > bounding_box[0] && vtk_pt[0] < bounding_box[1] &&
+        vtk_pt[1] > bounding_box[2] && vtk_pt[1] < bounding_box[3] &&
+        vtk_pt[2] > bounding_box[4] && vtk_pt[2] < bounding_box[5]) {
+      // This vertex should be included, so initialize its entry:
+      double normals[3];
+      normalsGeneric->GetTuple(0, normals);
+      for (int k = 0; k < 3; k++) {
+        vertexDataSets->at(k_good_vertices).v[k] = vtk_pt[k];
+        vertexDataSets->at(k_good_vertices).n[k] = normals[k];
+      }
+      vertexDataSets->at(k_good_vertices).color_observations.clear();
+      vertIndToDatasetInd->insert(std::pair<int, int>(i, k_good_vertices));
+      k_good_vertices++;
+    }
+  }
+}
+
 static int DoMain(void) {
   RemoteTreeViewerWrapper rm;
   auto console = spdlog::get("console");
@@ -86,15 +201,32 @@ static int DoMain(void) {
   string posegraph_filename =
       FLAGS_labelfusion_data_dir + "/posegraph.posegraph";
   auto tfs = GeneratePoseList(posegraph_filename);
-  console->info("Loaded {0:d} poses from {1:s}", tfs.size(), posegraph_filename);
+  console->info("Loaded {0:d} poses from {1:s}", tfs.size(),
+                posegraph_filename);
+
+  // Extract camera intrinsics matrix
+  string config_filename = FLAGS_labelfusion_data_dir + "/info.yaml";
+  Matrix3d K = GenerateCameraIntrinsics(config_filename);
+  console->info("Loaded camera intrinsics:");
+  std::cout << K << std::endl;
 
   string images_directory = FLAGS_labelfusion_data_dir + "/images/";
 
   // Open mesh with VTK
   vtkSmartPointer<vtkPolyData> meshPolyData =
       ReadPolyData(FLAGS_mesh_filename.c_str());
-  console->info("Loaded {0:d} points from {1:s}", meshPolyData->GetNumberOfPoints(),
-                FLAGS_mesh_filename);
+  console->info("Loaded {0:d} points from {1:s}",
+                meshPolyData->GetNumberOfPoints(), FLAGS_mesh_filename);
+
+  // Initialize the list of vertex sample info
+  // Hack a bounding box for now, this should be a config parameter.
+  double bb[6] = {-0.3, 0.3, -0.2, 0.2, 0.8, 1.1};
+  vector<VertexDataSet> vertexDataSets;
+  std::map<int, int> vertIndToDatasetInd;
+  InitializeVertexDataSets(meshPolyData, bb, &vertexDataSets, &vertIndToDatasetInd);
+  console->info("Initialized {0:d} vertex data sets", vertexDataSets.size());
+
+
 
   return 0;
 }
