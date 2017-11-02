316346c1abd3af53216ac83b4d9031578790cbf1
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-11-02 09:55:25 -0400

    fix build issues with camera_config package

--- src/catkin_projects/camera_config/src/camera_config/camera_info_publisher.py
+++ src/catkin_projects/camera_config/src/camera_config/camera_info_publisher.py
@@ -5,15 +5,7 @@ class CameraInfoPublisher:
 
 	def __init__(self):
 		self.cameraName = '/camera'
-		self.cameraInfoFilename = rospy.get_param(~camera_info_filename)
+		self.cameraInfoFilename = rospy.get_param('~camera_info_filename')
 
-		rospy.loginfo("camera_info_filename  = %s", camera_info_filename)
+		rospy.loginfo("camera_info_filename  = %s", self.cameraInfoFilename)
 		rospy.loginfo("finished initializing the node")
-
-
-
-if __name__=="__main__":
-
-	rospy.init_node("camera_info_publisher")
-
-	rospy.spin
