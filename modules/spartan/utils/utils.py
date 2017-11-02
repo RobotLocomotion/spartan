58750b5dffaac8533d85d7a6f6ba4ae076fefa3c
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-11-02 13:24:11 -0400

    tf bugs resolved, pointcloud visualizing correctly in rviz

--- modules/spartan/utils/utils.py
+++ modules/spartan/utils/utils.py
@@ -4,6 +4,9 @@ import collections
 import yaml
 import os
 
+# director
+from director import transformUtils
+
 
 def getSpartanSourceDir():
     return os.getenv("SPARTAN_SOURCE_DIR")
@@ -15,6 +18,39 @@ def getDictFromYamlFilename(filename):
 def saveToYaml(data, filename):
     with open(filename, 'w') as outfile:
         yaml.dump(data, outfile, default_flow_style=False)
+
+def poseFromTransform(transform):
+    pos, quat = transformUtils.poseFromTransform(transform)
+    pos = pos.tolist()
+    quat = quat.tolist()
+    d = dict()
+    d['translation'] = dict()
+    d['translation']['x'] = pos[0]
+    d['translation']['y'] = pos[1]
+    d['translation']['z'] = pos[2]
+
+    d['quaternion'] = dict()
+    d['quaternion']['w'] = quat[0]
+    d['quaternion']['x'] = quat[1]
+    d['quaternion']['y'] = quat[2]
+    d['quaternion']['z'] = quat[3]
+
+    return d
+
+def transformFromPose(d):
+    pos = [0]*3
+    pos[0] = d['translation']['x']
+    pos[1] = d['translation']['y']
+    pos[2] = d['translation']['z']
+
+    quatDict = getQuaternionFromDict(d)
+    quat = [0]*4
+    quat[0] = quatDict['w']
+    quat[1] = quatDict['x']
+    quat[2] = quatDict['y']
+    quat[3] = quatDict['z']
+
+    return transformUtils.transformFromPose(pos, quat)
 
 def getQuaternionFromDict(d):
     quat = None
