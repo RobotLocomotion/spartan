e29ac4a1c752f38f59e5bf0bb43a667ad4ad9f21
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-11-02 19:19:40 -0400

    code cleanup

--- modules/spartan/calibration/handeyecalibration.py
+++ modules/spartan/calibration/handeyecalibration.py
@@ -47,7 +47,6 @@ except:
 
 # spartan
 import spartan.utils.utils as spartanUtils
-import spartan.utils.director_utils as spartanDirectorUtils
 import spartan.utils.ros_utils as spartanROSUtils
 from spartan.utils.taskrunner import TaskRunner
 
@@ -238,7 +237,7 @@ class HandEyeCalibration(object):
         data['pose'] = self.robotService.getPose().tolist()
         data['hand_frame_name'] = self.handFrame
         handTransform = self.robotSystem.robotStateModel.getLinkFrame(self.handFrame)
-        data['hand_frame'] = spartanDirectorUtils.poseFromTransform(handTransform)
+        data['hand_frame'] = spartanUtils.poseFromTransform(handTransform)
         data['utime'] = self.robotSystem.robotStateJointController.lastRobotStateMessage.utime
         return data
 
