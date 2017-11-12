8d5d005d267e0b09b77cde71538e47899fdee97a
ChristopherSweeney <csweeney541@gmail.com>
2017-11-12 22:17:30 +0000

    adding ipynb and normal.py for depth return calibration

--- src/DepthSim/python/normal.py
+++ src/DepthSim/python/normal.py
@@ -14,6 +14,7 @@ import Image
 from vtk.util.misc import vtkRegressionTestImage
 import common as common
 
+#TODO:one shading lib for both depth and normal
 def set_shader_input(mapper):
   '''
   // Modify the shader to color based on model normal
