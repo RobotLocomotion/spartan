8cb4723de644d921683096d75483ecfb60294119
Gregory Izatt <gregory.izatt@gmail.com>
2017-12-04 20:03:14 -0500

    Add sim restarting ability

--- src/catkin_projects/drake_simulation/scripts/pybullet_passive_simulation_from_config.py
+++ src/catkin_projects/drake_simulation/scripts/pybullet_passive_simulation_from_config.py
@@ -6,6 +6,7 @@
 # Arguments:
 #   config: Configuration file
 #   rate: Desired real-time rate (will not run faster, might run slower)
+# Press q to quit and r to restart the simulation.
 
 import argparse
 import os
@@ -22,59 +23,80 @@ if __name__ == "__main__":
 
     # Set up a simulation with a ground plane and desired timestep
     physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-    p.setGravity(0,0,-9.81)
-    p.setTimeStep(args.timestep)
-
-    # Read in configuration file
-    config = yaml.load(open(args.config))
-
-    print config
-
-    if config["with_ground"] == True:
-        p.loadURDF(os.environ["SPARTAN_SOURCE_DIR"] + "/build/bullet3/data/plane.urdf")
-
-    # Models entry is a dictionary of model URDF strings
-    model_dict = config["models"]
-
-    # Add each model as requested
-    drake_resource_root = os.environ["DRAKE_RESOURCE_ROOT"]
-    ids = []
-    for instance in config["instances"]:
-        urdf = drake_resource_root + "/" + model_dict[instance["model"]]
-        q0 = instance["q0"]
-        position = q0[0:3]
-        quaternion = p.getQuaternionFromEuler(q0[3:8])
-        print "URDF ", urdf, " q0", q0
-        ids.append(p.loadURDF(urdf, position, quaternion))
         
-    # Run simulation with time control
-    start_time = time.time()
-    sim_time = 0.0
-    avg_sim_rate = -1.
-    sim_rate_RC = 0.1 # RC time constant for estimating sim rate
-    sim_rate_alpha = args.timestep / (sim_rate_RC + args.timestep) 
-    last_print_time = time.time() - 100
-    while True:
-        # Do a sim step
-        start_step_time = time.time()
-        p.stepSimulation()
-        end_step_time = time.time()
-        sim_time += args.timestep
+    keep_simulating = True
+    while keep_simulating:
+        p.resetSimulation()
 
-        # Estimate sim rate
-        this_sim_step_rate = args.timestep / (end_step_time - start_step_time)
-        if avg_sim_rate < 0:
-            # Initialization case
-            avg_sim_rate = this_sim_step_rate
-        else:
-            avg_sim_rate = this_sim_step_rate * sim_rate_alpha + (1. - sim_rate_alpha) * avg_sim_rate
+        p.setGravity(0,0,-9.81)
+        p.setTimeStep(args.timestep)
 
-        # Sleep if we're running too fast
-        elapsed = end_step_time - start_time
-        target_sim_time = elapsed * args.rate
-        if sim_time > target_sim_time:
-            time.sleep(sim_time - target_sim_time)
+        # Read in configuration file
+        config = yaml.load(open(args.config))
 
-        if time.time() - last_print_time > 1.0:
-            last_print_time = time.time()
-            print "Overall sim rate: ", sim_time / target_sim_time, ", current sim rate: ", avg_sim_rate, " at timestep ", args.timestep
+        print config
+
+        if config["with_ground"] == True:
+            p.loadURDF(os.environ["SPARTAN_SOURCE_DIR"] + "/build/bullet3/data/plane.urdf")
+
+        # Models entry is a dictionary of model URDF strings
+        model_dict = config["models"]
+
+        # Add each model as requested
+        drake_resource_root = os.environ["DRAKE_RESOURCE_ROOT"]
+        ids = []
+        for instance in config["instances"]:
+            urdf = drake_resource_root + "/" + model_dict[instance["model"]]
+            q0 = instance["q0"]
+            position = q0[0:3]
+            quaternion = p.getQuaternionFromEuler(q0[3:8])
+            print "URDF ", urdf, " q0", q0
+            ids.append(p.loadURDF(urdf, position, quaternion))
+            
+        # Run simulation with time control
+        start_time = time.time()
+        sim_time = 0.0
+        avg_sim_rate = -1.
+        sim_rate_RC = 0.1 # RC time constant for estimating sim rate
+        sim_rate_alpha = args.timestep / (sim_rate_RC + args.timestep) 
+        last_print_time = time.time() - 100
+
+        keep_going = True
+        while keep_going:
+            # Do a sim step
+            start_step_time = time.time()
+            p.stepSimulation()
+            end_step_time = time.time()
+            sim_time += args.timestep
+
+            # Estimate sim rate
+            this_sim_step_rate = args.timestep / (end_step_time - start_step_time)
+            if avg_sim_rate < 0:
+                # Initialization case
+                avg_sim_rate = this_sim_step_rate
+            else:
+                avg_sim_rate = this_sim_step_rate * sim_rate_alpha + (1. - sim_rate_alpha) * avg_sim_rate
+
+            # Sleep if we're running too fast
+            elapsed = end_step_time - start_time
+            target_sim_time = elapsed * args.rate
+            if sim_time > target_sim_time:
+                time.sleep(sim_time - target_sim_time)
+
+            if time.time() - last_print_time > 0.1:
+                last_print_time = time.time()
+                print "Overall sim rate: ", sim_time / target_sim_time, ", current sim rate: ", avg_sim_rate, " at time ", sim_time
+
+            events = p.getKeyboardEvents()
+            for key in events.keys():
+                if events[key] & p.KEY_WAS_TRIGGERED:
+                    if key == ord('q'):
+                        print "Quitting"
+                        keep_going = False
+                        keep_simulating = False
+                    elif key == ord('r'):
+                        print "Restarting"
+                        keep_going = False
+                        break
+
+
