caa762955fb1fa318f61e836fb76f205489b109b
peteflorence <pete.florence@gmail.com>
2017-12-21 23:09:29 +0000

    provide perform_elastic_fusion service

--- src/catkin_projects/fusion_server/scripts/capture_scene_and_fuse_client.py
+++ src/catkin_projects/fusion_server/scripts/capture_scene_and_fuse_client.py
@@ -12,7 +12,7 @@ def capture_scene_and_fuse_client():
     try:
         capture_scene_and_fuse = rospy.ServiceProxy('capture_scene_and_fuse', CaptureSceneAndFuse)
         resp1 = capture_scene_and_fuse()
-        return resp1.data_filepath
+        return resp1.pointcloud_filepath
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
