4ea1877e05eebf43d302f893b10dd97eb5743e42
manuelli <lucas.t.manuelli@gmail.com>
2018-01-02 19:07:58 -0500

    add forces_pro generated files

---
+++ src/ContactParticleFilter/bin/forcespro_qp_2_contact_point/interface/forcespro_qp_2_contact_point_mex.c
@@ -0,0 +1,250 @@
+/*
+forcespro_qp_2_contact_point : A fast customized optimization solver.
+
+Copyright (C) 2013-2017 EMBOTECH AG [info@embotech.com]. All rights reserved.
+
+
+This software is intended for simulation and testing purposes only. 
+Use of this software for any commercial purpose is prohibited.
+
+This program is distributed in the hope that it will be useful.
+EMBOTECH makes NO WARRANTIES with respect to the use of the software 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. 
+
+EMBOTECH shall not have any liability for any damage arising from the use
+of the software.
+
+This Agreement shall exclusively be governed by and interpreted in 
+accordance with the laws of Switzerland, excluding its principles
+of conflict of laws. The Courts of Zurich-City shall have exclusive 
+jurisdiction in case of any dispute.
+
+*/
+
+#include "mex.h"
+#include "math.h"
+#include "../include/forcespro_qp_2_contact_point.h"
+#include <stdio.h>
+
+
+
+/* copy functions */
+void copyCArrayToM(double *src, double *dest, solver_int32_default dim) 
+{
+    while (dim--) 
+	{
+        *dest++ = (double)*src++;
+    }
+}
+void copyMArrayToC(double *src, double *dest, solver_int32_default dim) 
+{
+    while (dim--) 
+	{
+        *dest++ = (double) (*src++) ;
+    }
+}
+
+
+
+
+/* Some memory for mex-function */
+forcespro_qp_2_contact_point_params params;
+forcespro_qp_2_contact_point_output output;
+forcespro_qp_2_contact_point_info info;
+
+/* THE mex-function */
+void mexFunction( solver_int32_default nlhs, mxArray *plhs[], solver_int32_default nrhs, const mxArray *prhs[] )  
+{
+	/* file pointer for printing */
+	FILE *fp = NULL;
+
+	/* define variables */	
+	mxArray *par;
+	mxArray *outvar;
+	const mxArray *PARAMS = prhs[0];
+	double *pvalue;
+	solver_int32_default i;
+	solver_int32_default exitflag;
+	const solver_int8_default *fname;
+	const solver_int8_default *outputnames[1] = {"alpha"};
+	const solver_int8_default *infofields[16] = { "it", "it2opt", "res_eq", "res_ineq",  "pobj",  "dobj",  "dgap", "rdgap",  "mu",  "mu_aff",  "sigma",  "lsit_aff",  "lsit_cc",  "step_aff",   "step_cc",  "solvetime"};
+	
+	/* Check for proper number of arguments */
+    if (nrhs != 1) 
+	{
+        mexErrMsgTxt("This function requires exactly 1 input: PARAMS struct.\nType 'help forcespro_qp_2_contact_point_mex' for details.");
+    }    
+	if (nlhs > 3) 
+	{
+        mexErrMsgTxt("This function returns at most 3 outputs.\nType 'help forcespro_qp_2_contact_point_mex' for details.");
+    }
+
+	/* Check whether params is actually a structure */
+	if( !mxIsStruct(PARAMS) ) 
+	{
+		mexErrMsgTxt("PARAMS must be a structure.");
+	}
+
+	/* copy parameters into the right location */
+	par = mxGetField(PARAMS, 0, "QuadraticCost");
+#ifdef MEXARGMUENTCHECKS
+    if( par == NULL )	
+	{
+        mexErrMsgTxt("PARAMS.QuadraticCost not found");
+    }
+    if( !mxIsDouble(par) )
+    {
+    mexErrMsgTxt("PARAMS.QuadraticCost must be a double.");
+    }
+    if( mxGetM(par) != 8 || mxGetN(par) != 8 ) 
+	{
+    mexErrMsgTxt("PARAMS.QuadraticCost must be of size [8 x 8]");
+    }
+#endif	 
+    copyMArrayToC(mxGetPr(par), params.QuadraticCost, 64);
+
+	par = mxGetField(PARAMS, 0, "LinearCost");
+#ifdef MEXARGMUENTCHECKS
+    if( par == NULL )	
+	{
+        mexErrMsgTxt("PARAMS.LinearCost not found");
+    }
+    if( !mxIsDouble(par) )
+    {
+    mexErrMsgTxt("PARAMS.LinearCost must be a double.");
+    }
+    if( mxGetM(par) != 8 || mxGetN(par) != 1 ) 
+	{
+    mexErrMsgTxt("PARAMS.LinearCost must be of size [8 x 1]");
+    }
+#endif	 
+    copyMArrayToC(mxGetPr(par), params.LinearCost, 8);
+
+	#if forcespro_qp_2_contact_point_SET_PRINTLEVEL > 0
+		/* Prepare file for printfs */
+		/*fp = freopen("stdout_temp","w+",stdout);*/
+        fp = fopen("stdout_temp","w+");
+		if( fp == NULL ) 
+		{
+			mexErrMsgTxt("freopen of stdout did not work.");
+		}
+		rewind(fp);
+	#endif
+
+	/* call solver */
+	exitflag = forcespro_qp_2_contact_point_solve(&params, &output, &info, fp);
+
+	/* close stdout */
+	/* fclose(fp); */
+	
+	#if forcespro_qp_2_contact_point_SET_PRINTLEVEL > 0
+		/* Read contents of printfs printed to file */
+		rewind(fp);
+		while( (i = fgetc(fp)) != EOF ) 
+		{
+			mexPrintf("%c",i);
+		}
+		fclose(fp);
+	#endif
+
+	/* copy output to matlab arrays */
+	plhs[0] = mxCreateStructMatrix(1, 1, 1, outputnames);
+	outvar = mxCreateDoubleMatrix(8, 1, mxREAL);
+	copyCArrayToM( output.alpha, mxGetPr(outvar), 8);
+	mxSetField(plhs[0], 0, "alpha", outvar);	
+
+	/* copy exitflag */
+	if( nlhs > 1 )
+	{
+		plhs[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(plhs[1]) = (double)exitflag;
+	}
+
+	/* copy info struct */
+	if( nlhs > 2 )
+	{
+        plhs[2] = mxCreateStructMatrix(1, 1, 16, infofields);
+         
+		
+		/* iterations */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = (double)info.it;
+		mxSetField(plhs[2], 0, "it", outvar);
+
+		/* iterations to optimality (branch and bound) */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = (double)info.it2opt;
+		mxSetField(plhs[2], 0, "it2opt", outvar);
+		
+		/* res_eq */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.res_eq;
+		mxSetField(plhs[2], 0, "res_eq", outvar);
+
+		/* res_ineq */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.res_ineq;
+		mxSetField(plhs[2], 0, "res_ineq", outvar);
+
+		/* pobj */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.pobj;
+		mxSetField(plhs[2], 0, "pobj", outvar);
+
+		/* dobj */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.dobj;
+		mxSetField(plhs[2], 0, "dobj", outvar);
+
+		/* dgap */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.dgap;
+		mxSetField(plhs[2], 0, "dgap", outvar);
+
+		/* rdgap */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.rdgap;
+		mxSetField(plhs[2], 0, "rdgap", outvar);
+
+		/* mu */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.mu;
+		mxSetField(plhs[2], 0, "mu", outvar);
+
+		/* mu_aff */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.mu_aff;
+		mxSetField(plhs[2], 0, "mu_aff", outvar);
+
+		/* sigma */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.sigma;
+		mxSetField(plhs[2], 0, "sigma", outvar);
+
+		/* lsit_aff */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = (double)info.lsit_aff;
+		mxSetField(plhs[2], 0, "lsit_aff", outvar);
+
+		/* lsit_cc */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = (double)info.lsit_cc;
+		mxSetField(plhs[2], 0, "lsit_cc", outvar);
+
+		/* step_aff */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.step_aff;
+		mxSetField(plhs[2], 0, "step_aff", outvar);
+
+		/* step_cc */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.step_cc;
+		mxSetField(plhs[2], 0, "step_cc", outvar);
+
+		/* solver time */
+		outvar = mxCreateDoubleMatrix(1, 1, mxREAL);
+		*mxGetPr(outvar) = info.solvetime;
+		mxSetField(plhs[2], 0, "solvetime", outvar);
+	}
+}
