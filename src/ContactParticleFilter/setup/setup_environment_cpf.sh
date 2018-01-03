1aa185f4cf22cbf344e292c4860f5782e4e8f5b3
manuelli <lucas.t.manuelli@gmail.com>
2018-01-03 14:27:14 -0500

    add custom sourcing file

---
+++ src/ContactParticleFilter/setup/setup_environment_cpf.sh
@@ -0,0 +1,5 @@
+export GUROBI_HOME="/home/manuelli/software_tools/gurobi752/linux64"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+export GRB_LICENSE_FILE="/home/manuelli/software_tools/gurobi.lic"
+export PYTHONPATH="/home/manuelli/forces_pro:${PYTHONPATH}"
