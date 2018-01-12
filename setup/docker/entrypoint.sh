dd89f8b8f12763151f893cea5ef806e8d028ba89
manuelli <lucas.t.manuelli@gmail.com>
2018-01-12 13:26:18 -0500

    add use_cpf convenience function to entrypoint

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -16,9 +16,14 @@ function use_spartan_ros()
 	. ~/spartan/build/catkin_projects/devel/setup.bash
 }
 
+function use_cpf()
+{
+    . ~/spartan/src/ContactParticleFilter/setup/setup_environment_cpf.sh
+}
 export -f use_spartan
 export -f use_ros
 export -f use_spartan_ros
+export -f use_cpf
 
 exec "$@"
 
