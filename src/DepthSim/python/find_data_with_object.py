0ba150c7e2febff96ebce403329640252e4bbf00
gizatt <gizatt@mit.edu>
2018-01-19 12:20:22 -0500

    refactored computer graphics code

--- src/DepthSim/python/find_data_with_object.py
+++ src/DepthSim/python/find_data_with_object.py
@@ -1,6 +1,10 @@
 import os 
 import numpy as np
 from scipy import misc
+from common import common
+import yaml
+from render import render_sim
+from director import vtkAll as vtk
 
 path  = "/home/drc/spartan/Data_ALL/logs_test/"
 paths = []
@@ -8,10 +12,37 @@ for f in os.listdir(path):
 	if "2017" in f:
 		if "registration_result.yaml" in os.listdir(path+f):
 			with open(path+f+"/registration_result.yaml") as read:
-				if "drill" in read.read():
+				transformYaml = yaml.load(read)
+				if len(transformYaml.keys())==1 and transformYaml.keys()[0]=="drill":
 					paths.append(f) 
 print paths
-for i in paths:
-	#os.system("directorPython depth.py /home/drc/spartan/Data_ALL/logs_test/"+i+"/" 4000 'None' "/home/drc/spartan/Data_ALL/drill/")
 
-#do all drill and real depth data for all images
+view_height = 640
+view_width = 480
+renderer = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+interactor = vtk.vtkRenderWindowInteractor()
+renWin.SetSize(view_height,view_width)
+camera = vtk.vtkCamera()
+renderer.SetActiveCamera(camera);
+renWin.AddRenderer(renderer);
+interactor.SetRenderWindow(renWin);
+common.set_up_camera_params(camera)
+
+use_mesh = False
+out_dir = "/home/drc/spartan/Data_ALL/gan_data/sim/train/"
+
+mesh = "None"
+for i in paths:
+
+  data_dir = path+i
+  data_dir_name =  os.path.basename(os.path.normpath(data_dir))
+  num_im = 4000
+
+
+  print "rendering Label Fusion data", data_dir_name
+  render_sim.render_depth(renWin,renderer,camera,data_dir,data_dir_name,num_im,mesh,out_dir,use_mesh,view_height,view_width)
+
+
+renWin.Render();
+interactor.Start();
--- src/DepthSim/python/normalize_im.py
+++ src/DepthSim/python/normalize_im.py
@@ -3,11 +3,16 @@ import numpy as np
 from scipy import misc
 
 path  = "/media/drc/DATA1/chris_labelfusion/logs_test/test_gan/train_real/"
-#path  = "/media/drc/DATA1/chris_labelfusion/logs_test/test_gan_sim/train_log/"
+path1  = "/media/drc/DATA1/chris_labelfusion/logs_test/test_gan_sim/train_log/"
+
 for f in os.listdir(path):
-	n = misc.imread(path+f)
-	n = misc.imresize(n,(100,100))
-	n=n/float(np.max(n))
-	print n
-	break
-	misc.imsave(path+f, n)
+	if ".png" in f:
+		n = misc.imread(path+f)
+		n=n/float(np.max(n))
+		misc.imsave(path+f, n)
+
+for f in os.listdir(path1):
+	if ".png" in f:
+		n = misc.imread(path1+f)
+		n=n/float(np.max(n))
+		misc.imsave(path1+f, n)
