b645bd1af18aa94aa08b2e20d91455881ea0de54
manuelli <lucas.t.manuelli@gmail.com>
2018-02-14 17:45:20 -0500

    add handical repo, update gitignore, add some calibration targets

---
+++ setup/docker/install_handical_dependencies.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euxo pipefail
+root_dir=$(pwd)
+
+mkdir handical_dependencies
+workdir=root_dir/handical_dependencies
+
+
+
+apt-get update
+apt install --no-install-recommends \
+  libopencv-dev \
+  python-opencv
+
+install_gtsam()
+{   cd $workdir
+    git clone https://thduynguyen@bitbucket.org/thduynguyen/gtsam-duy.git
+    cd gtsam-duy
+    gtsam_source_dir = $(pwd)
+    git fetch && git checkout feature/wrap_cal3ds2
+    pip install -r cython/requirements.txt # install cython requirements
+
+    mkdir build
+    cd build
+    mkdir install
+    gtsam_install_dir=$gtsam_source_dir/build/install
+    cmake -DGTSAM_INSTALL_CYTHON_TOOLBOX=ON -DCMAKE_INSTALL_PREFIX=$gtsam_install_dir ..
+    make -j4
+    make install -j4
+
+    echo export GTSAM_INSTALL_DIR=$(pwd) >> ~/.bashrc
+
+    # add install/bin to PATH
+    # add install/cython to PYTHONPATH
+
+    # use something like 
+    # 
+    # echo 'export PYTHONPATH=$PYTHONPATH:$(openrave-config --python-dir)' >> $root_dir/.bashrc
+
+}
+
