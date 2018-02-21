75dc4052bd5d9e55912a872d5baf5cabc98b5919
Yunzhu Li <liyunzhu@mit.edu>
2018-02-21 15:07:42 -0500

    script now write out to yaml file in right place

--- modules/spartan/calibration/intrinsics_calibration.py
+++ modules/spartan/calibration/intrinsics_calibration.py
@@ -3,13 +3,9 @@ import argparse
 import yaml
 import spartan.utils.utils as spartanUtils
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--camera_name', default='xtion_pro')
-parser.add_argument('--rgb', default=False, action='store_true')
-parser.add_argument('--ir', default=False, action='store_true')
-args = parser.parse_args()
 
-def extract_intrinsics(path_to_tar_file, path_to_camera_info_yaml_file):
+def extract_intrinsics(path_to_tar_file, path_to_camera_info_yaml_file,
+                       camera_name):
     cmd = 'mkdir -p /tmp/calibrationdata'
     os.system(cmd)
     cmd = 'tar xvf ' + path_to_tar_file + ' -C ' + '/tmp/calibrationdata' + ' > /dev/null 2>&1'
@@ -19,9 +15,32 @@ def extract_intrinsics(path_to_tar_file, path_to_camera_info_yaml_file):
 
     print camera_info_dict
 
+    camera_info_dict['camera_name'] = camera_name
+
+    spartanUtils.saveToYaml(camera_info_dict, path_to_camera_info_yaml_file)
+
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--camera_name', default='xtion_pro')
+    parser.add_argument('--rgb', default=False, action='store_true')
+    parser.add_argument('--ir', default=False, action='store_true')
+    args = parser.parse_args()
+
+    camera_info_filename = ""
+    if args.ir:
+        camera_info_filename = 'depth_camera_info.yaml'
+    elif args.rgb:
+        camera_info_filename = 'rgb_camera_info.yaml'
+    else:
+        print "Need to pass either --rgb or --ir"
+        exit(0)
+
     path_to_camera_info_yaml_file = os.path.join(spartanUtils.getSpartanSourceDir(),
-                 'src/catkin_projects/camera_config/data', args.camera_name, 'master')
+                 'src/catkin_projects/camera_config/data', args.camera_name,
+                 'master', camera_info_filename)
+
     path_to_tar_file = '/tmp/calibrationdata.tar.gz'
-    extract_intrinsics(path_to_tar_file, path_to_camera_info_yaml_file)
+    extract_intrinsics(path_to_tar_file, path_to_camera_info_yaml_file,
+                       args.camera_name)
