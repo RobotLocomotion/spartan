041f61bdffda8e2b4d2431db3b69f5bb4e681fc9
Pete Florence <pete.florence@gmail.com>
2018-02-22 13:22:43 -0500

    handical builds

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -16,9 +16,17 @@ function use_spartan_ros()
 	. ~/spartan/build/catkin_projects/devel/setup.bash
 }
 
+function use_handical()
+{
+	GTSAM_INSTALL_DIR=~/handical_dependencies/gtsam-duy/build/install
+	export PATH=$PATH:$GTSAM_INSTALL_DIR/bin
+	export PYTHONPATH=$PYTHONPATH:$GTSAM_INSTALL_DIR/cython
+}
+
 export -f use_spartan
 export -f use_ros
 export -f use_spartan_ros
+export -f use_handical
 
 exec "$@"
 
