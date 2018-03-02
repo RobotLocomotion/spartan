e81f867d4084d0ba7cf25b8ec37b246ac17694e4
manuelli <lucas.t.manuelli@gmail.com>
2018-03-02 18:44:27 -0500

    code cleanup

--- src/catkin_projects/fusion_server/scripts/test_image_capture.py
+++ src/catkin_projects/fusion_server/scripts/test_image_capture.py
@@ -2,10 +2,24 @@ import fusion_server.fusion as fusion
 import rospy
 
 def main():
-    image_capture = fusion.ImageCapture()
+
+    rgb_topic = "/camera_carmine_1/rgb/image_rect_color"
+    depth_topic = "/camera_carmine_1/depth_registered/sw_registered/image_rect"
+    camera_info_topic = "/camera_carmine_1/rgb/camera_info"
+    rgb_encoding = 'bgr8'
+
+    camera_frame = "camera_carmine_1_rgb_optical_frame"
+    world_frame = "base"
+
+
     ros_bag_filename = "/home/manuelli/spartan/sandbox/open3d/kuka_scene/fusion1519825352.8.bag"
     output_dir = "/home/manuelli/spartan/sandbox/open3d/kuka_scene/extracted_images"
     
+    image_capture = fusion.ImageCapture(rgb_topic, depth_topic, camera_info_topic,
+        camera_frame, world_frame, output_dir, rgb_encoding=rgb_encoding)
+
+
+
     image_capture.load_ros_bag(ros_bag_filename)
     image_capture.process_ros_bag(image_capture.ros_bag, output_dir)
     # image_capture.start()
