6143fac654b69a8f386d2454464741f8b972b3af
manuelli <lucas.t.manuelli@gmail.com>
2018-06-18 16:53:45 -0400

    update test tsdf fusion script

--- src/catkin_projects/fusion_server/scripts/test_tsdf_fusion.py
+++ src/catkin_projects/fusion_server/scripts/test_tsdf_fusion.py
@@ -5,15 +5,17 @@ import os
 from fusion_server.fusion import ImageCapture
 import fusion_server.tsdf_fusion as tsdf_fusion
 
-rgb_topic = "/camera_carmine_1/rgb/image_rect_color"
-depth_topic = "/camera_carmine_1/depth_registered/sw_registered/image_rect"
-camera_info_topic = "/camera_carmine_1/rgb/camera_info"
-camera_frame = "camera_carmine_1_rgb_optical_frame"
-world_frame = "base"
+# rgb_topic = "/camera_carmine_1/rgb/image_rect_color"
+# depth_topic = "/camera_carmine_1/depth_registered/sw_registered/image_rect"
+# camera_info_topic = "/camera_carmine_1/rgb/camera_info"
+# camera_frame = "camera_carmine_1_rgb_optical_frame"
+# world_frame = "base"
 
-data_folder = "/home/manuelli/spartan/data_volume/fusion/test/05_drill_long_downsampled"
-image_folder = os.path.join(data_folder, 'images')
-bag_filepath = os.path.join(data_folder, "fusion.bag")
+data_folder = "/home/manuelli/spartan/data_volume/pdc/logs_special/static_scenes/2018-05-07-22-04-57"
+
+processed_dir = os.path.join(data_folder, 'processed')
+images_dir = os.path.join(processed_dir, 'images')
+# bag_filepath = os.path.join(data_folder, "fusion.bag")
 
 def extract_images_from_rosbag():
     image_capture = ImageCapture(rgb_topic, depth_topic, camera_info_topic,
@@ -24,15 +26,16 @@ def extract_images_from_rosbag():
     image_capture.process_ros_bag(image_capture.ros_bag, output_dir)
 
 def main():
-    # extract_images_from_rosbag()
+    print "preparing for tsdf_fusion for"
+    tsdf_fusion.format_data_for_tsdf(images_dir)
 
-    # tsdf_fusion.format_data_for_tsdf(image_folder)
-    tsdf_fusion.run_tsdf_fusion_cuda(image_folder)
+    print "running tsdf fusion"
+    tsdf_fusion.run_tsdf_fusion_cuda(images_dir)
 
-    tsdf_bin_file = os.path.join(data_folder, 'tsdf.bin')
-    tsdf_mesh_file = os.path.join(data_folder, 'fusion_mesh.ply')
-    
-    tsdf_fusion.convert_tsdf_to_ply(tsdf_bin_file, tsdf_mesh_file)
+    print "converting tsdf to ply"
+    tsdf_bin_filename = os.path.join(processed_dir, 'tsdf.bin')
+    tsdf_mesh_filename = os.path.join(processed_dir, 'fusion_mesh.ply')
+    tsdf_fusion.convert_tsdf_to_ply(tsdf_bin_filename, tsdf_mesh_filename)
 
 
 if __name__ == "__main__":
