8ab5d44ca69e0f467e751903db6424b3448f37e3
manuelli <lucas.t.manuelli@gmail.com>
2018-06-22 15:32:27 -0400

    add script to extract single scene

---
+++ src/catkin_projects/fusion_server/scripts/extract_and_fuse_single_scene.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import rospy
+import time
+import argparse
+
+from fusion_server.fusion import FusionServer
+import fusion_server.tsdf_fusion as tsdf_fusion
+
+import spartan.utils.utils as spartanUtils
+
+import batch_extract_and_fuse_all_scenes
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_dir", type=str, required=True)
+    
+    args = parser.parse_args()
+
+    batch_extract_and_fuse_all_scenes.extract_and_fuse_single_scene(args.log_dir, downsample=True):
+
