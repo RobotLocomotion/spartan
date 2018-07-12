81a4d265e19928b101a0c4a922fb631ad05d2723
Pang Tao <pangtao@mit.edu>
2018-07-12 19:57:13 -0400

    add TODO item: support for PlanStatus

--- src/catkin_projects/drake_robot_control/include/drake_robot_control/plan_base.h
+++ src/catkin_projects/drake_robot_control/include/drake_robot_control/plan_base.h
@@ -8,12 +8,23 @@ namespace robot_plan_runner {
 // Abstract class
 // Child classes of PlanBase should have a concrete Step() method which
 // generates the commanded state/torque.
+
+// TODO: add support for all Plans to return PlanStatus;
+enum PlanStatus {
+  NOT_STARTED,
+  RUNNING,
+  FINISHED_NORMALLY,
+  STOPPED_BY_EXTERNAL_TRIGGER,
+  STOPPED_BY_SAFETY_CHECK
+};
+
 class PlanBase {
  public:
   explicit PlanBase(std::shared_ptr<const RigidBodyTreed> tree)
       : tree_(std::move(tree)) {
     num_positions = tree_->get_num_positions();
     num_velocities = tree_->get_num_velocities();
+    plan_status_ = NOT_STARTED;
   }
 
   // x:=[q,v] robot state
@@ -26,13 +37,14 @@ class PlanBase {
                     Eigen::VectorXd *const tau_commanded) const = 0;
   int get_num_positions() const { return num_positions; }
   int get_num_velocities() const { return num_velocities; }
-
+  PlanStatus get_plan_status() const { return plan_status_; }
  protected:
   std::shared_ptr<const RigidBodyTreed> tree_;
 
  private:
   int num_positions;
   int num_velocities;
+  PlanStatus plan_status_;
 };
 
 } // namespace robot_plan_runner
