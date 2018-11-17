faeb161f4a331a6112acd9f4e08638839b8229c9
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2018-11-16 20:14:23 -0500

    adjust scan poses

--- src/catkin_projects/fusion_server/src/fusion_server/fusion.py
+++ src/catkin_projects/fusion_server/src/fusion_server/fusion.py
@@ -386,8 +386,8 @@ class FusionServer(object):
         pose_list.append(["close_up_scan", "right_3"])
         pose_list.append(["close_up_scan", "right_4"])
         pose_list.append(["close_up_scan", "far_right_1"])
-        pose_list.append(["close_up_scan", "far_right_2"])
-        pose_list.append(["close_up_scan", "far_right_1"])
+        # pose_list.append(["close_up_scan", "far_right_2"])
+        # pose_list.append(["close_up_scan", "far_right_1"])
         pose_list.append(["close_up_scan", "right_1"])
         pose_list.append(["close_up_scan", "center"])
         pose_list.append(["close_up_scan", "left_1"])
@@ -700,7 +700,8 @@ class FusionServer(object):
 
 
         # Start bagging for far out data collection
-        base_path = os.path.join(spartanUtils.getSpartanSourceDir(), 'data_volume', 'pdc', 'logs_proto')
+        # base_path = os.path.join(spartanUtils.getSpartanSourceDir(), 'data_volume', 'pdc', 'logs_proto')
+        base_path = os.path.join(spartanUtils.getSpartanSourceDir(), 'data_volume', 'pdc', 'logs_shoes')
         log_id_name = spartanUtils.get_current_YYYY_MM_DD_hh_mm_ss()
         log_subdir = "raw"
         bagfile_directory = os.path.join(base_path, log_id_name, log_subdir)
@@ -709,13 +710,13 @@ class FusionServer(object):
 
         print "moving robot through regular scan poses"
         self.start_bagging(full_path_to_bag_file=full_path_to_bagfile)
-        pose_list = self.config['scan']['pose_list_quick']
-
+        pose_list = self.config['scan']['pose_list']
         joint_positions = self.get_joint_positions_for_pose(pose_list[0])
         self.robotService.moveToJointPosition(joint_positions,
                                               maxJointDegreesPerSecond=self.config['speed']['scan'])
-        rospy.sleep(3.0)
-        # self._move_robot_through_pose_list(pose_list, randomize_wrist=True, hit_original_poses=True)
+        # rospy.sleep(3.0)
+        self._move_robot_through_pose_list(pose_list, randomize_wrist=True, hit_original_poses=True)
+
         self._stop_bagging()
 
 
@@ -736,7 +737,7 @@ class FusionServer(object):
         # now start bagging and move the robot through the poses
         self.start_bagging(full_path_to_bag_file=full_path_to_bagfile)
         self._move_robot_through_pose_list(pose_list, randomize_wrist=True, hit_original_poses=True)
-        rospy.sleep(3.0)
+        # rospy.sleep(3.0)
         self._stop_bagging()
         rospy.sleep(1.0)
 
