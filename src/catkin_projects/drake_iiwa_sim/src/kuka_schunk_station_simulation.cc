af8021a5f586141703c67e9e43eeb948e3d3d0cf
Gregory Izatt <gregory.izatt@gmail.com>
2018-11-20 16:37:33 -0500

    Clang-formatting

--- src/catkin_projects/drake_iiwa_sim/src/kuka_schunk_station_simulation.cc
+++ src/catkin_projects/drake_iiwa_sim/src/kuka_schunk_station_simulation.cc
@@ -4,8 +4,8 @@
 #include "common_utils/system_utils.h"
 
 #include "drake_iiwa_sim/kuka_schunk_station.h"
-#include "drake_iiwa_sim/schunk_wsg_ros_actionserver.h"
 #include "drake_iiwa_sim/ros_scene_graph_visualizer.h"
+#include "drake_iiwa_sim/schunk_wsg_ros_actionserver.h"
 
 #include "drake/common/eigen_types.h"
 #include "drake/common/find_resource.h"
@@ -61,34 +61,33 @@ int do_main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   YAML::Node station_config = YAML::LoadFile(FLAGS_config);
-  
 
   systems::DiagramBuilder<double> builder;
 
   // Create the Kuka + Schunk.
   auto station = builder.AddSystem<KukaSchunkStation>(
-    station_config, 0.002, IiwaCollisionModel::kPolytopeCollision);
+      station_config, 0.002, IiwaCollisionModel::kPolytopeCollision);
 
   // Add a work table in front of the robot, and to its side.
   const double dz_table_top_robot_base = 0.736 + 0.057 / 2.;
   const std::string table_sdf_path = FindResourceOrThrow(
-      "drake/examples/kuka_iiwa_arm/models/table/extra_heavy_duty_table_surface_only_collision.sdf");
+      "drake/examples/kuka_iiwa_arm/models/table/"
+      "extra_heavy_duty_table_surface_only_collision.sdf");
   auto plant = &station->get_mutable_multibody_plant();
   auto scene_graph = &station->get_mutable_scene_graph();
   const auto table_front =
       AddModelFromSdfFile(table_sdf_path, "table_front", plant);
   plant->WeldFrames(
       plant->world_frame(), plant->GetFrameByName("link", table_front),
-      RigidTransform<double>(
-          Eigen::Vector3d(0.75, 0, -dz_table_top_robot_base))
+      RigidTransform<double>(Eigen::Vector3d(0.75, 0, -dz_table_top_robot_base))
           .GetAsIsometry3());
   const auto table_left =
       AddModelFromSdfFile(table_sdf_path, "table_left", plant);
-  plant->WeldFrames(
-      plant->world_frame(), plant->GetFrameByName("link", table_left),
-      RigidTransform<double>(
-          Eigen::Vector3d(0.0, 0.8, -dz_table_top_robot_base))
-          .GetAsIsometry3());
+  plant->WeldFrames(plant->world_frame(),
+                    plant->GetFrameByName("link", table_left),
+                    RigidTransform<double>(
+                        Eigen::Vector3d(0.0, 0.8, -dz_table_top_robot_base))
+                        .GetAsIsometry3());
 
   // TODO(gizatt) Merge into Schunk station, or its own class?
   // Assembly a list of initializations
@@ -104,33 +103,38 @@ int do_main(int argc, char* argv[]) {
     const auto pose = node["q0"].as<std::vector<double>>();
     Eigen::Vector3d xyz(pose[0], pose[1], pose[2]);
     Eigen::Vector3d rpy(pose[3], pose[4], pose[5]);
-    Eigen::Isometry3d object_tf(RigidTransform<double>(RollPitchYaw<double>(rpy), xyz).GetAsIsometry3());
+    Eigen::Isometry3d object_tf(
+        RigidTransform<double>(RollPitchYaw<double>(rpy), xyz)
+            .GetAsIsometry3());
 
     const auto object_class = node["model"].as<std::string>();
-          auto object_class_node = station_config["models"][object_class];
-          DRAKE_DEMAND(object_class_node);
-          std::string full_path = expandEnvironmentVariables(object_class_node.as<std::string>());
-          // TODO: replace with unique name
+    auto object_class_node = station_config["models"][object_class];
+    DRAKE_DEMAND(object_class_node);
+    std::string full_path =
+        expandEnvironmentVariables(object_class_node.as<std::string>());
+    // TODO: replace with unique name
     std::stringstream model_name;
     model_name << node["model"].as<std::string>() << "_" << k++;
     std::string body_name = node["body_name"].as<std::string>();
-    printf("Substr: %s", full_path.substr(full_path.size()-4).c_str());
+    printf("Substr: %s", full_path.substr(full_path.size() - 4).c_str());
     drake::multibody::ModelInstanceIndex model_instance;
-    if (full_path.substr(full_path.size()-4) == "urdf"){  
+    if (full_path.substr(full_path.size() - 4) == "urdf") {
       model_instance = AddModelFromUrdfFile(full_path, model_name.str(), plant);
     } else {
       model_instance = AddModelFromSdfFile(full_path, model_name.str(), plant);
     }
-          
+
     if (node["fixed"].as<bool>()) {
-         // Cludgy, but default behavior in AddModelFromSdfFile is to
-         // make a frame at the root of the added model with the same name
-         // as the added model.
-         plant->WeldFrames(plant->world_frame(), plant->GetBodyByName(body_name, model_instance).body_frame(),
-               object_tf);
+      // Cludgy, but default behavior in AddModelFromSdfFile is to
+      // make a frame at the root of the added model with the same name
+      // as the added model.
+      plant->WeldFrames(
+          plant->world_frame(),
+          plant->GetBodyByName(body_name, model_instance).body_frame(),
+          object_tf);
     } else {
       initializations_to_do.push_back(
-        ObjectInitializationInfo({model_instance, body_name, object_tf}));
+          ObjectInitializationInfo({model_instance, body_name, object_tf}));
     }
   }
 
@@ -139,9 +143,9 @@ int do_main(int argc, char* argv[]) {
   // TODO(gizatt) Merge this into the Schunk Station, or its own
   // class?
   {
-
     auto render_scene_graph =
-        builder.template AddSystem<drake::geometry::dev::SceneGraph>(*scene_graph);
+        builder.template AddSystem<drake::geometry::dev::SceneGraph>(
+            *scene_graph);
 
     if (station_config["cameras"]) {
       for (const auto camera_config : station_config["cameras"]) {
@@ -150,24 +154,29 @@ int do_main(int argc, char* argv[]) {
         DRAKE_DEMAND(camera_config["config_base_dir"]);
         YAML::Node camera_extrinsics_yaml =
             YAML::LoadFile(expandEnvironmentVariables(
-    camera_config["config_base_dir"].as<std::string>() +
-                           "/camera_info.yaml"));
+                camera_config["config_base_dir"].as<std::string>() +
+                "/camera_info.yaml"));
         YAML::Node rgb_camera_info_yaml =
             YAML::LoadFile(expandEnvironmentVariables(
-    camera_config["config_base_dir"].as<std::string>() +
-                           "/rgb_camera_info.yaml"));
+                camera_config["config_base_dir"].as<std::string>() +
+                "/rgb_camera_info.yaml"));
         YAML::Node depth_camera_info_yaml =
             YAML::LoadFile(expandEnvironmentVariables(
-    camera_config["config_base_dir"].as<std::string>() +
-                           "/depth_camera_info.yaml"));
+                camera_config["config_base_dir"].as<std::string>() +
+                "/depth_camera_info.yaml"));
 
         auto camera_name = camera_config["name"].as<std::string>();
         drake::geometry::dev::render::DepthCameraProperties camera_properties(
             640, 480, M_PI_4, geometry::dev::render::Fidelity::kLow, 0.1, 2.0);
-  const auto body_node_index = plant->GetBodyByName(
-    camera_config["mounting_body_name"].as<std::string>(),
-    plant->GetModelInstanceByName(camera_config["mounting_model_name"].as<std::string>())).index();
-    const auto depth_camera_frame_id = plant->GetBodyFrameIdOrThrow(body_node_index);
+        const auto body_node_index =
+            plant
+                ->GetBodyByName(
+                    camera_config["mounting_body_name"].as<std::string>(),
+                    plant->GetModelInstanceByName(
+                        camera_config["mounting_model_name"].as<std::string>()))
+                .index();
+        const auto depth_camera_frame_id =
+            plant->GetBodyFrameIdOrThrow(body_node_index);
 
         const auto tf_yaml =
             camera_extrinsics_yaml["depth"]["extrinsics"]
@@ -182,8 +191,8 @@ int do_main(int argc, char* argv[]) {
                             tf_yaml["translation"]["z"].as<double>()));
         auto camera =
             builder.template AddSystem<systems::sensors::dev::RgbdCamera>(
-                camera_name, depth_camera_frame_id, depth_camera_tf.GetAsIsometry3(),
-                camera_properties, false);
+                camera_name, depth_camera_frame_id,
+                depth_camera_tf.GetAsIsometry3(), camera_properties, false);
         builder.Connect(render_scene_graph->get_query_output_port(),
                         camera->query_object_input_port());
 
@@ -200,8 +209,8 @@ int do_main(int argc, char* argv[]) {
   // Visualizers
   geometry::ConnectDrakeVisualizer(&builder, station->get_scene_graph(),
                                    station->GetOutputPort("pose_bundle"));
-  auto ros_visualizer = builder.AddSystem<RosSceneGraphVisualizer>(
-    station->get_scene_graph());
+  auto ros_visualizer =
+      builder.AddSystem<RosSceneGraphVisualizer>(station->get_scene_graph());
   builder.Connect(station->GetOutputPort("pose_bundle"),
                   ros_visualizer->get_pose_bundle_input_port());
 
@@ -213,7 +222,8 @@ int do_main(int argc, char* argv[]) {
   auto iiwa_command_subscriber = builder.AddSystem(
       systems::lcm::LcmSubscriberSystem::Make<drake::lcmt_iiwa_command>(
           "IIWA_COMMAND", &lcm));
-  auto iiwa_command = builder.AddSystem<examples::kuka_iiwa_arm::IiwaCommandReceiver>();
+  auto iiwa_command =
+      builder.AddSystem<examples::kuka_iiwa_arm::IiwaCommandReceiver>();
   builder.Connect(iiwa_command_subscriber->get_output_port(),
                   iiwa_command->get_input_port(0));
 
@@ -226,7 +236,8 @@ int do_main(int argc, char* argv[]) {
   builder.Connect(iiwa_command->get_commanded_torque_output_port(),
                   station->GetInputPort("iiwa_feedforward_torque"));
 
-  auto iiwa_status = builder.AddSystem<examples::kuka_iiwa_arm::IiwaStatusSender>();
+  auto iiwa_status =
+      builder.AddSystem<examples::kuka_iiwa_arm::IiwaStatusSender>();
   // The IiwaStatusSender input port wants size 14, but only uses the first 7.
   // TODO(russt): Consider cleaning up the IiwaStatusSender.
   auto zero_padding =
@@ -251,8 +262,7 @@ int do_main(int argc, char* argv[]) {
                   iiwa_status_publisher->get_input_port());
 
   auto wsg_ros_actionserver = builder.AddSystem<SchunkWsgActionServer>(
-    "/wsg50_driver/wsg50/gripper_control/",
-    "/wsg50_driver/wsg50/status");
+      "/wsg50_driver/wsg50/gripper_control/", "/wsg50_driver/wsg50/status");
   builder.Connect(wsg_ros_actionserver->get_position_output_port(),
                   station->GetInputPort("wsg_position"));
   builder.Connect(wsg_ros_actionserver->get_force_limit_output_port(),
@@ -278,15 +288,15 @@ int do_main(int argc, char* argv[]) {
   const VectorXd qdot0 = VectorXd::Zero(7);
   station->SetIiwaVelocity(qdot0, &station_context);
 
-// Yuck, repeating this iteration from above is unfortunate.
-// Maybe store a dict up above of initial poses we should set,
-// or see if I can do this before finalization is done on the plant diagram?
+  // Yuck, repeating this iteration from above is unfortunate.
+  // Maybe store a dict up above of initial poses we should set,
+  // or see if I can do this before finalization is done on the plant diagram?
   for (const auto& initialization : initializations_to_do) {
-      plant->tree().SetFreeBodyPoseOrThrow(
-        plant->GetBodyByName(initialization.body_name, initialization.model_instance),
+    plant->tree().SetFreeBodyPoseOrThrow(
+        plant->GetBodyByName(initialization.body_name,
+                             initialization.model_instance),
         initialization.tf,
-        &station->GetMutableSubsystemContext(
-                    *plant, &station_context));
+        &station->GetMutableSubsystemContext(*plant, &station_context));
   }
 
   simulator.set_publish_every_time_step(false);
@@ -300,6 +310,4 @@ int do_main(int argc, char* argv[]) {
 }  // namespace
 }  // namespace drake_iiwa_sim
 
-int main(int argc, char* argv[]) {
-  return drake_iiwa_sim::do_main(argc, argv);
-}
+int main(int argc, char* argv[]) { return drake_iiwa_sim::do_main(argc, argv); }
