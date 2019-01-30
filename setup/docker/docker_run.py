cd1b32cdd35607fe4b37c890f12811dd8d812cdd
Gregory Izatt <gregory.izatt@gmail.com>
2019-01-29 19:03:17 -0500

    More jenkins tweaks

--- setup/docker/docker_run.py
+++ setup/docker/docker_run.py
@@ -22,6 +22,8 @@ if __name__=="__main__":
 
 	parser.add_argument("-nodudp", "--no_udp", action='store_true', help="(optional) don't expose the udp ports")
 
+	parser.add_argument("--no_nvidia", action='store_true', help="(optional) use docker rather than nvidia-docker (for CI)")
+
 	args = parser.parse_args()
 	print("running docker container derived from image %s" %args.image)
 	source_dir=os.getcwd()
@@ -31,7 +33,10 @@ if __name__=="__main__":
 	spartan_source_dir = os.path.join(home_directory, 'spartan')
 
 	cmd = "xhost +local:root \n"
-	cmd += "nvidia-docker run "
+	if args.no_nvidia:
+		cmd += "docker run "
+	else:
+		cmd += "nvidia-docker run "
 	if args.container:
 		cmd += " --name %(container_name)s " % {'container_name': args.container}
 
