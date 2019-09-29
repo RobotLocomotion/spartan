2069a80ddc8b977af9e3d16fa3159ec28f021078
wei <gaowei19951004@gmail.com>
2019-09-29 00:16:43 -0400

    update config

--- setup/docker/entrypoint.sh
+++ setup/docker/entrypoint.sh
@@ -33,7 +33,7 @@ function use_handical()
 
 function use_python_packages()
 {
-	export PYTHONPATH="~/spartan/src/catkin_projects/pyrunner_ros:$PYTHONPATH"
+	export PYTHONPATH=$PYTHONPATH:$SPARTAN_SOURCE_DIR/src/catkin_projects/pyrunner_ros
 }
 
 function use_spartan()
