dddced586691e25712709fdb018177fd0d799ecb
manuelli <lucas.t.manuelli@gmail.com>
2020-06-30 11:51:18 -0400

    update script

--- modules/spartan/key_dynam/copy_processed_logs.py
+++ modules/spartan/key_dynam/copy_processed_logs.py
@@ -3,32 +3,34 @@ from __future__ import print_function
 import argparse
 import os
 import shutil
+from zipfile import ZipFile
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("--logs_dir", type=str, required=True)
-    # parser.add_argument("--dest_dir", type=str, required=True)
-    args = parser.parse_args()
+def zip_and_copy():
+
+    # src_dir = "/home/manuelli/data/key_dynam/hardware_experiments/logs/push_box_good"
+    # dst_dir = "/media/manuelli/Extreme SSD/key_dynam/hardware_experiments/push_box_hardware"
+
+    src_dir = "/media/manuelli/Extreme SSD/key_dynam/hardware_experiments/push_box_hardware"
+    dst_dir = "/home/manuelli/data/key_dynam/dataset/push_box_hardware"
 
 
-
-    # logs_dir = args.logs_dir
-    logs_dir = "/home/manuelli/data/key_dynam/hardware_experiments/logs/push_box_good"
-    logs_list = os.listdir(logs_dir)
+    logs_list = os.listdir(src_dir)
     logs_list.sort()
-    dst_dir = "/media/manuelli/Extreme SSD/key_dynam/hardware_experiments/push_box_hardware"
-    # dst_dir = args.dest_dir
+
 
     for log_name in logs_list:
         if os.path.exists(os.path.join(dst_dir, log_name)):
             print("already copied log %s, skipping" %(log_name))
             continue
+        else:
+            print("processing log: %s" %(log_name))
 
-        processed_dir = os.path.join(logs_dir, log_name, "processed")
-        processed_zip = os.path.join(logs_dir, log_name, "processed.zip")
+        processed_dir = os.path.join(src_dir, log_name, "processed")
+        processed_zip = os.path.join(src_dir, log_name, "processed.zip")
 
         if not os.path.exists(processed_zip):
             print("making zip file")
+            # base name should have been nothing . . .
             shutil.make_archive(base_name=processed_dir, format="zip", base_dir=processed_dir)
             print("processed_dir", processed_dir)
 
@@ -38,6 +40,46 @@ if __name__ == "__main__":
 
         os.makedirs(os.path.dirname(dst_zip))
         shutil.copyfile(processed_zip, dst_zip)
+
+
+def unzip_logs():
+    logs_dir = "/home/manuelli/data/key_dynam/dataset/push_box_hardware"
+    logs_list = os.listdir(logs_dir)
+    logs_list.sort()
+
+    for log_name in logs_list:
+        tmp_dir = os.path.join(logs_dir, log_name, 'tmp')
+        processed_dir = os.path.join(logs_dir, log_name, "processed")
+        processed_zip = os.path.join(logs_dir, log_name, "processed.zip")
+
+        if os.path.isdir(processed_dir):
+            print("Already extracted, skipping: %s" %(log_name))
+            continue
+        else:
+            print("Extracting log: %s" %(log_name))
+
+        # see https://stackoverflow.com/questions/3451111/unzipping-files-in-python
+        with ZipFile(processed_zip, 'r') as zf:
+            zf.extractall(tmp_dir)
+            zf.close()
+
+        # basename = "media/manuelli/Extreme SSD/key_dynam/hardware_experiments/push_box_hardware"
+        basename = "home/manuelli/data/key_dynam/hardware_experiments/logs/push_box_good"
+
+        src = os.path.join(logs_dir, log_name, 'tmp', basename, log_name, 'processed')
+        dst = processed_dir
+        # print("moving %s:%s" %(src,dst))
+        shutil.move(src, dst)
+        shutil.rmtree(os.path.join(logs_dir, log_name, 'tmp'))
+
+        # just try one to start
+
+
+
+
+if __name__ == "__main__":
+    # zip_and_copy()
+    unzip_logs()
 
 
 
