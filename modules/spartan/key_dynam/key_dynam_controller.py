41bcbd13e95977b1c56917a837c0d32dbc836e45
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2020-07-08 13:12:51 -0400

    closed loop plan working

--- modules/spartan/key_dynam/key_dynam_controller.py
+++ modules/spartan/key_dynam/key_dynam_controller.py
@@ -217,6 +217,8 @@ class KeyDynamController(PlanarMouseTeleop):
         print("resp_data.keys()", resp_data.keys())
         print("action_seq\n", resp_data['action_seq'])
 
+
+
         s = raw_input("\nexecute plan? y/n\n")
         if s != "y":
             print("aborting")
@@ -225,6 +227,8 @@ class KeyDynamController(PlanarMouseTeleop):
             print("executing plan")
 
         rate = rospy.Rate(self._control_rate)
+
+
         msg = self.make_empty_cartesian_goal_point_msg()
         action_seq = resp_data['action_seq']
         self._task_space_streaming.start_streaming()
@@ -254,6 +258,132 @@ class KeyDynamController(PlanarMouseTeleop):
 
         print("plan finished")
 
+    def test_control_rate(self):
+
+        # send the plan msg
+        plan_msg_file = "/home/manuelli/data/key_dynam/sandbox/2020-07-07-20-09-54_push_right_box_horizontal/plan_msg.p"
+        plan_msg = spartan_utils.load_pickle(plan_msg_file)
+        self.send_single_frame_plan(msg=plan_msg)
+
+
+        print("\n---COMPUTE_CONTROL_ACTION REQUEST----")
+
+        data = self.collect_latest_data()
+        msg = {'type': 'COMPUTE_CONTROL_ACTION',
+               'data': data,
+               'debug': 1, # False gets converted to a string, maybe by our convert function
+                }
+        start_time = time.time()
+        self._zmq_client.send_data(msg)
+        resp = self._zmq_client.recv_data()
+        # this the
+
+        resp_data = resp['data']
+        elapsed = time.time() - start_time
+        self._mouse_manager.release_mouse_focus()
+        print("computing plan took %.3f seconds" % (elapsed))
+        print("resp_data.keys()", resp_data.keys())
+        print("action_seq\n", resp_data['action_seq'])
+
+        rate = rospy.Rate(self._control_rate)
+        msg = self.make_empty_cartesian_goal_point_msg()
+        action_seq = resp_data['action_seq']
+
+        prev_time = time.time()
+        while True:
+            data = self.collect_latest_data()
+            msg = {'type': 'COMPUTE_CONTROL_ACTION',
+                   'data': data,
+                   'debug': 0,
+                   }
+            self._zmq_client.send_data(msg)
+            resp = self._zmq_client.recv_data()
+            print("elapsed: %.3f" %(time.time() - prev_time))
+            prev_time = time.time()
+            rate.sleep()
+
+
+    def execute_plan_closed_loop(self):
+
+
+        print("\n---EXECUTING CLOSED LOOP PLAN----")
+
+        data = self.collect_latest_data()
+        msg = {'type': 'COMPUTE_CONTROL_ACTION',
+               'data': data,
+               'debug': 1, # send number instead
+                }
+        start_time = time.time()
+        self._zmq_client.send_data(msg)
+        resp = self._zmq_client.recv_data()
+        resp_data = resp['data']
+        elapsed = time.time() - start_time
+        self._mouse_manager.release_mouse_focus()
+
+        print("computing plan took %.3f seconds" % (elapsed))
+        print("resp_data.keys()", resp_data.keys())
+        print("action_seq\n", resp_data['action_seq'])
+
+        s = raw_input("\nexecute plan? y/n\n")
+        if s != "y":
+            print("aborting")
+            return
+        else:
+            print("executing plan")
+
+        self._task_space_streaming.start_streaming()
+
+        rate = rospy.Rate(self._control_rate)
+        control_msg = self.make_empty_cartesian_goal_point_msg()
+        action_seq = resp_data['action_seq']
+
+        prev_time = time.time()
+        while True:
+            data = self.collect_latest_data()
+            msg = {'type': 'COMPUTE_CONTROL_ACTION',
+                   'data': data,
+                   'debug': 0,
+                   }
+            self._zmq_client.send_data(msg)
+            resp = self._zmq_client.recv_data()
+
+            if resp['type'] != "CONTROL_ACTION":
+                print("got message of type %s, stopping streaming" %(resp['type']))
+                self._task_space_streaming.stop_streaming()
+                break
+
+            # extract the control action
+            action = resp['data']['action']
+            print("action", action)
+            control_msg.setpoint_linear_velocity.x = action[0]
+            control_msg.setpoint_linear_velocity.y = action[1]
+
+            # check if it's a safe action to send
+            if not self._software_safety.check_message(control_msg):
+                print("unsafe control, breaking off")
+                self._task_space_streaming.stop_streaming()
+                break
+
+            self._task_space_streaming.publish(control_msg)
+            rate.sleep()
+
+        self._task_space_streaming.stop_streaming()
+
+
+    def reset_to_start_position(self,):
+        """
+        - First moves the robot to a safe position above the tabl
+        - Next moves robot to starting position
+        :return:
+        """
+        self.stop_teleop()
+        # pose = self._stored_poses_dict["key_dynam"]["above_table_ready_pose"]
+        # success = self._robotService.moveToJointPosition(pose, maxJointDegreesPerSecond=30, timeout=5)
+
+        pose = self._stored_poses_dict['debug']['key_dynam_start_pose_open_loop_plan']
+        success = self._robotService.moveToJointPosition(pose, maxJointDegreesPerSecond=30, timeout=5)
+
+
     def run(self):
         """
         Runs main loop at given speed, 5 Hz
@@ -268,10 +398,6 @@ class KeyDynamController(PlanarMouseTeleop):
         finally:
             controller._mouse_manager.release_mouse_focus()
 
-    def debug_action(self):
-        self.send_single_frame_plan()
-        self.send_control_action_req()
-
 
 def debug(controller):
     plan_msg_file = "/home/manuelli/data/key_dynam/sandbox/2020-07-07-20-09-54_push_right_box_horizontal/plan_msg.p"
@@ -282,17 +408,27 @@ def debug(controller):
     compute_control_action_msg = spartan_utils.load_pickle(compute_control_action_msg_file)
     controller.execute_plan_open_loop(msg=compute_control_action_msg)
 
+def test_closed_loop_control(controller):
+    plan_msg_file = "/home/manuelli/data/key_dynam/sandbox/2020-07-07-20-09-54_push_right_box_horizontal/plan_msg.p"
+    plan_msg = spartan_utils.load_pickle(plan_msg_file)
+    controller.send_single_frame_plan(msg=plan_msg)
+
+    controller.reset_to_start_position()
+    controller.execute_plan_closed_loop()
 
 if __name__ == "__main__":
     rospy.init_node('key_dynam_controller', anonymous=True)
-
     config_file = os.path.join(spartan_utils.getSpartanSourceDir(),'modules/spartan/key_dynam/controller_config.yaml')
     config = spartan_utils.getDictFromYamlFilename(config_file)
     controller = KeyDynamController(config)
 
     try:
         # debug(controller)
-        controller.run()
+        # controller.run()
+
+        controller._mouse_manager.release_mouse_focus()
+        test_closed_loop_control(controller)
+        # controller.test_control_rate()
     finally:
         controller._mouse_manager.release_mouse_focus()
 
